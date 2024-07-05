4946256283343b7f863d51ecb92280d80d3e241f
Nirma Abro <NirmaAbro143@gmail.com>
2024-07-05 16:06:34 -0600

    authentication successfully done

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require("./routes/AuthRouter");
+const { connectMongoDB } = require("./config/connection");
+require("dotenv").config();
+console.log("JWT_SECRET:", process.env.JWT_SECRET); // Add this line to check if the secret is being read
+
+
+const app = express();
+
+require("dotenv").config();
+// MongoDB connection
+connectMongoDB("mongodb://127.0.0.1:27017/Recipies");
+const PORT = process.env.PORT || 8080;
+
+app.get("/ping", (req, res) => {
+  res.send("PONG");
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/auth", AuthRouter);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
